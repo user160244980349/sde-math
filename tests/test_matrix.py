385be108892e3c6f8eda7fc213ee9f27ef2e9166
user160244980349 <mkuznetsov7991@gmail.com>
2020-07-11 15:52:35 +0000

    Yet another tests

---
+++ tests/test_matrix.py
@@ -0,0 +1,38 @@
+import unittest
+from numpy import array
+from numpy.testing import assert_array_equal
+
+from mathematics.matrix import vec_to_eye, extending_assignment
+
+
+class Matrix(unittest.TestCase):
+    def test_vec_to_eye(self):
+
+        vec = array([1, 2, 3])
+        mat = vec_to_eye(vec)
+        e_mat = array([[1, 0, 0],
+                       [0, 2, 0],
+                       [0, 0, 3]])
+
+        self.assertEqual(assert_array_equal(mat, e_mat), None)
+
+    def test_extending_assignment(self):
+
+        mat = array([[]])
+        mat = extending_assignment(mat, 0, 0, 1)
+        mat = extending_assignment(mat, 0, 5, 1)
+        mat = extending_assignment(mat, 5, 0, 1)
+        mat = extending_assignment(mat, 5, 5, 1)
+
+        e_mat = array([[1, 0, 0, 0, 0, 1],
+                       [0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0],
+                       [1, 0, 0, 0, 0, 1]])
+
+        self.assertEqual(assert_array_equal(mat, e_mat), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
