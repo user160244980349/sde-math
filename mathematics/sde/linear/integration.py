16374feb47d4334910654ecbfb89e1db13ff4b42
user160244980349 <mkuznetsov7991@gmail.com>
2020-07-20 17:38:51 +0000

    Comment covariance

--- mathematics/sde/linear/integration.py
+++ mathematics/sde/linear/integration.py
@@ -7,14 +7,15 @@ from mathematics.matrix import diagonal_to_column
 
 class Integral:
     def __init__(self, n):
-        self.n, self.t0, self.tk, self.dt, self.integration_step = n, 0, 0, 0, 0
-        self.mat_a, self.mat_ad, self.mat_bd, self.mat_h, self.mat_fd = \
-            None, None, None, None, None
+        self.n, self.t0, self.tk, self.dt, self.integration_step = \
+            n, 0, 0, 0, 0
+        self.mat_a, self.mat_ad, self.mat_bd, self.mat_h, self.mat_fd, self.distortion = \
+            None, None, None, None, None, None
         self.mat_x0, self.mat_mx0, self.mat_dx0, self.mat_xt, self.mat_mx, self.mat_dx = \
             None, None, None, ndarray((n, 0)), ndarray((n, 0)), ndarray((n, 0))
-        self.vec_yt, self.vec_my, self.vec_dy, self.vec_t, self.vec_ry = \
-            [], [], [], [], []
-        self.distortion = None
+        self.vec_yt, self.vec_my, self.vec_dy, self.vec_t = \
+            [], [], [], []
+        # self.vec_ry = []
 
     def integrate(self):
         higher_limit = self.integration_step + int((self.tk - self.t0) / self.dt + 1)
