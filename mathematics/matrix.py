67e51397ac62be449e78099d5946490ba927eb65
user160244980349 <mkuznetsov7991@gmail.com>
2020-07-25 22:48:28 +0000

    Operators are tested

--- mathematics/matrix.py
+++ mathematics/matrix.py
@@ -1,20 +1,8 @@
-from numpy import shape, vstack, hstack, zeros
+from numpy import shape, zeros
 
 
 class NotASquareMatrix(Exception):
     pass
-
-
-def extending_assignment(matrix, i, j, value):
-    if shape(matrix)[0] <= i:
-        matrix = vstack((matrix, zeros((i - shape(matrix)[0] + 1, shape(matrix)[1]))))
-
-    if shape(matrix)[1] <= j:
-        matrix = hstack((matrix, zeros((shape(matrix)[0], j - shape(matrix)[1] + 1))))
-
-    matrix[i][j] = value
-
-    return matrix
 
 
 def vec_to_eye(vector):
--- mathematics/sde/linear/stoch.py
+++ mathematics/sde/linear/stoch.py
@@ -1,7 +1,7 @@
 from numpy import transpose, zeros, ndarray, sqrt
 from numpy.linalg import eig
 
-from mathematics.matrix import extending_assignment, vec_to_eye
+from mathematics.matrix import vec_to_eye
 from mathematics.sde.linear.dindet import dindet
 
 
@@ -22,62 +22,98 @@ def algorithm_11_2(n: int, mat_a: ndarray, mat_f: ndarray, dt: float):
 
 
 def algorithm_11_3(n: int, mat_g: ndarray):
-    mat_vec = ndarray((1, 1))
-
+    # calculating dimensions sizes
+    # these are complicated thoughts
+    # about indices just leave as they are
     i2 = 0
-
+    v_size = 0
     for i in range(n):
         n2 = n - i
-
         for j in range(n2):
-            mat_vec = extending_assignment(mat_vec, j + i2, 0, mat_g[j][j + i])
+            if v_size < j + i2:
+                v_size = j + i2
+        i2 = i2 + n - i
 
+    mat_vec = ndarray((v_size + 1, 1))
+
+    # actual algorithm
+    i2 = 0
+    for i in range(n):
+        n2 = n - i
+        for j in range(n2):
+            mat_vec[j + i2][0] = mat_g[j][j + i]
         i2 = i2 + n - i
 
     return mat_vec
 
 
 def algorithm_11_4(n: int, mat_dv: ndarray):
-    mat_d1 = ndarray((1, 1))
-
+    # calculating dimensions sizes
+    # these are complicated thoughts
+    # about indices just leave as they are
     i2 = 0
-
+    size = 0
     for i in range(n):
         n2 = n - i
-
         for j in range(n2):
-            mat_d1 = extending_assignment(mat_d1, j, j + i, mat_dv[j + i2][0])
-            mat_d1 = extending_assignment(mat_d1, j + i, j, mat_dv[j + i2][0])
+            if size < j + i:
+                size = j + i
+        i2 = i2 + n - i
 
+    mat_d1 = ndarray((size + 1, size + 1))
+
+    # actual algorithm
+    i2 = 0
+    for i in range(n):
+        n2 = n - i
+        for j in range(n2):
+            mat_d1[j][j + i] = mat_dv[j + i2][0]
+            mat_d1[j + i][j] = mat_dv[j + i2][0]
         i2 = i2 + n - i
 
     return mat_d1
 
 
 def algorithm_11_5(n: int, mat_a: ndarray):
-    mat_one1 = zeros((n, n))
-    mat_ac = ndarray((1, 1))
-
+    # calculating dimensions sizes
+    # these are complicated thoughts
+    # about indices just leave as they are
     r = 0
+    v_size = 0
+    h_size = 0
 
     for i in range(n):
         n2 = n - i
+        for j in range(n2):
+            o = 0
+            for k in range(n):
+                n3 = n - k
+                for m in range(n3):
+                    if v_size < m + o:
+                        v_size = m + o
+                    if h_size < r:
+                        h_size = r
+                o = o + n - k
+            r = r + 1
 
+    mat_one1 = zeros((n, n))
+    mat_ac = ndarray((v_size + 1, h_size + 1))
+
+    # actual algorithm
+    r = 0
+    for i in range(n):
+        n2 = n - i
         for j in range(n2):
             i2 = j + i
             mat_one1[j][i2] = 1
             mat_one1[i2][j] = 1
             mat_one_a = mat_one1.dot(transpose(mat_a)) + mat_a.dot(mat_one1)
             o = 0
-
             for k in range(n):
                 n3 = n - k
-
                 for m in range(n3):
-                    mat_ac = extending_assignment(mat_ac, m + o, r, mat_one_a[m][m + k])
-
+                    mat_ac[m + o][r] = mat_one_a[m][m + k]
                 o = o + n - k
-
             mat_one1 = zeros((n, n))
             r = r + 1
 
