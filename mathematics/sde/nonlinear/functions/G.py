20910c415f1246f7f56556e5c3e537de13eade8a
user160244980349 <mkuznetsov7991@gmail.com>
2020-08-03 02:17:25 +0000

    Milstein method ready, Euler beautify, speed improvements, some operators are fixed

--- mathematics/sde/nonlinear/functions/G.py
+++ mathematics/sde/nonlinear/functions/G.py
@@ -1,6 +1,7 @@
 from sympy import Function, Transpose, MatMul
 
 from mathematics.sde.nonlinear.functions.Grad import Grad
+from mathematics.sde.nonlinear.functions.Unwrap import Unwrap
 
 
 class G(Function):
@@ -9,4 +10,4 @@ class G(Function):
     @classmethod
     def eval(cls, c, f, dx):
         if c.is_Matrix and not c.is_symbol and not f.is_symbol:
-            return MatMul(Transpose(c), Grad(f, dx))[0, 0]
+            return Unwrap(MatMul(Transpose(c), Grad(f, dx)))
